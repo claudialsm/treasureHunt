8a4d2a1bdf0a88448f5bd26487382ca35e8a3b80
claudialsm <68153639+claudialsm@users.noreply.github.com>
2022-11-03 14:29:51 +0000

    Add dummy changes to test integration with Jenkins

--- src/main/java/com/lesson/treasureHunt/Player.java
+++ src/main/java/com/lesson/treasureHunt/Player.java
@@ -9,5 +9,6 @@ public class Player extends Unit {
 //	Dummy change from Claudia to test Jenkins
 //	Dummy change from Claudia to test Jenkins #2
 //	Dummy change from Claudia to test Jenkins #3
+//	Dummy change from Claudia to test Jenkins #4
 	 
 }
